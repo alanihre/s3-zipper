f6f61a120f95f90a22ef666d15b6a36c65ceb926
Alan Ihre <alan.ihre@alanihre.se>
2020-08-01 23:39:25 +0200

    Added example main.go.

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/alanihre/s3zipper"
+)
+
+func main() {
+	// Create a new AWS session. Credentials stored in environment variables
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-north-1"),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	inputBucket := "my-s3-bucket"
+	files := []string{"file1.txt", "file2.txt"}
+	archiveBucket := "my-s3-bucket"
+	archiveFileName := "archive.zip"
+
+	err = s3zipper.S3Zip(sess, inputBucket, files, archiveBucket, archiveFileName)
+	if err != nil {
+		panic(err)
+	}
+}
